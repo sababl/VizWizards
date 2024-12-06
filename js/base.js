b82b261e00c3725b052dc0e21caea6e9f0375a63
Saba <balal.saba@gmail.com>
2024-12-06 17:23:32 +0100

    change base template of pages

---
+++ js/base.js
@@ -0,0 +1,8 @@
+function toggleDropdown(sectionId) {
+    const dropdown = document.getElementById(sectionId);
+    if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+    } else {
+        dropdown.style.display = 'block';
+    }
+}

8cfc84d11ea61fd058631672dfa22d69c53a2932
Saba <balal.saba@gmail.com>
2025-02-07 11:36:56 +0100

    add Final Project to submenu

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,8 +1,17 @@
 function toggleDropdown(sectionId) {
     const dropdown = document.getElementById(sectionId);
+    
     if (dropdown.style.display === 'block') {
         dropdown.style.display = 'none';
     } else {
+        // Close other submenus under the same parent
+        const allDropdowns = dropdown.parentElement.querySelectorAll('.dropdown-content');
+        allDropdowns.forEach(menu => {
+            if (menu.id !== sectionId) {
+                menu.style.display = 'none';
+            }
+        });
+
         dropdown.style.display = 'block';
     }
 }
